b72b6862d753cc7e2ca4b0face1aa98efea5769b
Dest1n640 <realmadridfunca@gmail.com>
2026-07-27 11:43:38 +0700

    feat: add main window model

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-def main():
-    print("Hello from tui-vault!")
+from textual.app import App, ComposeResult
+from textual.widgets import Footer, Header
+
+
+class TuiVault(App):
+    """A Textual app to manage stopwatches."""
+
+    BINDINGS = [("q", "quit", "Quit")]
+
+    def compose(self) -> ComposeResult:
+        """Create child widgets for the app."""
+        yield Header()
+        yield Footer()
 
 
 if __name__ == "__main__":
-    main()
+    app = TuiVault()
+    app.run()
