7bff987d7b0a5eb9a9041be3b52acce9e43a7ee2
Dest1n640 <realmadridfunca@gmail.com>
2026-07-27 13:48:53 +0700

    fix: fix project name and fix ffi script

--- Test/test_ffi.py
+++ Test/test_ffi.py
@@ -1,3 +1,9 @@
-from example import ModifyString
+from pathlib import Path
+import sys
 
-print(ModifyString("Hello"))
+ROOT = Path(__file__).resolve().parent.parent
+sys.path.insert(0, str(ROOT / "core" / "build"))
+
+import core_module
+
+print(core_module.ModifyString("\nhello"))
--- core/src/crypto.cpp
+++ core/src/crypto.cpp
@@ -5,7 +5,7 @@ namespace py = pybind11;
 
 std::string ModifyString(std::string input) { return "Hello World " + input; }
 
-PYBIND11_MODULE(code_module, m, py::mod_gil_not_used()) {
+PYBIND11_MODULE(core_module, m, py::mod_gil_not_used()) {
   m.doc() = "Hello";
   m.def("ModifyString", &ModifyString, "A function that modify string");
 }
