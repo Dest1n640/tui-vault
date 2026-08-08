eeca65446db34553ce01d8f4ac16068640e95173
Dest1n640 <realmadridfunca@gmail.com>
2026-08-08 14:16:18 +0800

    feat: realize first crypto logic

--- core/src/crypto.cpp
+++ core/src/crypto.cpp
@@ -1,11 +1,24 @@
 #include <pybind11/pybind11.h>
+#include <sodium.h>
 #include <string>
+#include <vector>
+#include <spdlog/spdlog.h>
 
 namespace py = pybind11;
 
-std::string ModifyString(std::string input) { return "Hello World " + input; }
+std::vector<uint8_t> crypto_key(std::vector<uint8_t> key){
+  std::vector<uint8_t> vec;
+  auto sodium = sodium_init();
+  if (sodium < 0){
+    spdlog::error("Sodium initialization error");
+    return vec;
+  }
+  
+  auto crypto_key = crypto_pwhash(key);
+  return crypto_key;
+}
 
 PYBIND11_MODULE(core_module, m, py::mod_gil_not_used()) {
-  m.doc() = "Hello";
-  m.def("ModifyString", &ModifyString, "A function that modify string");
+  m.doc() = "Crypto key";
+  m.def("crypto_key", &crypto_key, "A function that hash key");
 }
