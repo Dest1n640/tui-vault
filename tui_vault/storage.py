24b1548d508e986256ce759416b67e9f7f9eb10f
Dest1n640 <realmadridfunca@gmail.com>
2026-08-25 15:21:57 +0800

    feat(storage): implement binary storage manager with AEAD encryption

---
+++ tui_vault/storage.py
@@ -0,0 +1,50 @@
+from pydantic import BaseModel
+from pathlib import Path
+import os
+import json
+
+from core_module import crypto_key, encrypt_data, decrypt_data
+
+
+class VaultItem(BaseModel):
+  service: str
+  login: str | None = None
+  password: str | None = None
+  notes: str | None = None
+
+
+class VaultManager:
+  SALT_LEN = 16
+  NONCE_LEN = 24
+
+  def __init__(self, filepath: Path):
+    self.filepath = filepath
+    self._key: list[int] | None = None
+    self._salt: bytes | None = None
+
+  def create_vault(self, master_password: str) -> None:
+    self._salt = os.urandom(self.SALT_LEN)
+    key = crypto_key(master_password, list(self._salt))
+    nonce, ciphertext = encrypt_data("[]", key)
+    self.filepath.write_bytes(self._salt + bytes(nonce) + bytes(ciphertext))
+
+  def unlock_vault(self, master_password: str) -> list[VaultItem]:
+    data = self.filepath.read_bytes()
+    if len(data) < self.SALT_LEN + self.NONCE_LEN + 16:
+      raise ValueError("Data size less the need")
+
+    self._salt = data[:self.SALT_LEN]
+    nonce = data[self.SALT_LEN: self.SALT_LEN + self.NONCE_LEN]
+    ciphertext = data[self.SALT_LEN + self.NONCE_LEN:]
+    self._key = crypto_key(master_password, list(self._salt))
+    raw_json = decrypt_data(list(ciphertext), self._key, list(nonce))
+    vault = [VaultItem.model_validate(item) for item in json.loads(raw_json)]
+    return vault
+
+  def save_vault(self, items: list[VaultItem]):
+    if self._key is None or self._salt is None:
+      raise RuntimeError("Vault is not unlocked or initialized")
+
+    raw_json = json.dumps([item.model_dump() for item in items])
+    nonce, ciphertext = encrypt_data(raw_json, self._key)
+    self.filepath.write_bytes(self._salt + bytes(nonce) + bytes(ciphertext))
