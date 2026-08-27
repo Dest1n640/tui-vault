954c7feea233b5dc06407c444873ed7b78d3a74f
Dest1n640 <realmadridfunca@gmail.com>
2026-08-27 18:37:05 +0800

    refactor: use ruff fix and ruff format

--- tests/test_crypto.py
+++ tests/test_crypto.py
@@ -1,18 +1,21 @@
-import sys
 import os
+import sys
 from pathlib import Path
+
 import pytest
 
 ROOT = Path(__file__).resolve().parent.parent
 sys.path.insert(0, str(ROOT / "core" / "build"))
 
-from core_module import crypto_key, encrypt_data, decrypt_data
+from core_module import crypto_key, decrypt_data, encrypt_data
+
 
 @pytest.fixture
 def master_key():
     password = "Master_password"
     salt = list(os.urandom(16))
     return crypto_key(password, salt)
+
 
 @pytest.mark.parametrize("invalid_salt_len", [0, 10, 15, 17, 32])
 def test_crypto_key_invalid_salt(invalid_salt_len):
@@ -22,15 +25,19 @@ def test_crypto_key_invalid_salt(invalid_salt_len):
         crypto_key(password, salt)
 
 
-@pytest.mark.parametrize("payload", [
-    "",
-    "Simple secret",
-    '{"service": "github", "login": "user", "password": "123"}',
-    "Тест с русскими буквами"
-])
+@pytest.mark.parametrize(
+    "payload",
+    [
+        "",
+        "Simple secret",
+        '{"service": "github", "login": "user", "password": "123"}',
+        "Тест с русскими буквами",
+    ],
+)
 def test_encryption_roundtrip(master_key, payload):
     nonce, ciphertext = encrypt_data(payload, master_key)
     decrypted = decrypt_data(ciphertext, master_key, nonce)
     assert decrypted == payload
+
 
 # TODO: дописать тесты для проверки целостности данных, аутентификации, передача шифротекста <16 байт
--- tui_vault/session.py
+++ tui_vault/session.py
@@ -2,59 +2,60 @@ from enum import Enum
 from pathlib import Path
 from uuid import UUID
 
-from storage import VaultManager, VaultItem
+from storage import VaultItem, VaultManager
 
 MAIN_DIRECTORY = Path.home() / ".config" / "tui_vault" / "vault.bin"
 
+
 class State(Enum):
-  LOCKED = 'Locked'
-  UNLOCKED = 'Unlocked'
-  FIRST_RUN = 'First_run'
+    LOCKED = "Locked"
+    UNLOCKED = "Unlocked"
+    FIRST_RUN = "First_run"
+
 
 class Session:
-  def __init__(self):
-    self.vault_manager = VaultManager(MAIN_DIRECTORY)
-    self.state = State.LOCKED if MAIN_DIRECTORY.exists() else State.FIRST_RUN
-    self.items = []
+    def __init__(self):
+        self.vault_manager = VaultManager(MAIN_DIRECTORY)
+        self.state = State.LOCKED if MAIN_DIRECTORY.exists() else State.FIRST_RUN
+        self.items = []
 
-  def create_session(self, master_password: str):
-    if self.state == State.FIRST_RUN:
-      self.vault_manager.create_vault(master_password)
-      self.state = State.UNLOCKED
-    
+    def create_session(self, master_password: str):
+        if self.state == State.FIRST_RUN:
+            self.vault_manager.create_vault(master_password)
+            self.state = State.UNLOCKED
 
-  def unlock(self, master_password: str):
-    if self.state == State.LOCKED:
-      self.items = self.vault_manager.unlock_vault(master_password)
-      self.state = State.UNLOCKED
-    return self.items
+    def unlock(self, master_password: str):
+        if self.state == State.LOCKED:
+            self.items = self.vault_manager.unlock_vault(master_password)
+            self.state = State.UNLOCKED
+        return self.items
 
-  def lock(self):
-    if self.state == State.UNLOCKED:
-      self.vault_manager.save_vault(self.items)
-      self.items = []
-      self.state = State.LOCKED
+    def lock(self):
+        if self.state == State.UNLOCKED:
+            self.vault_manager.save_vault(self.items)
+            self.items = []
+            self.state = State.LOCKED
 
-  def add_item(self, item: VaultItem):
-    if self.state == State.LOCKED:
-      raise ValueError("Session is locked")
-    self.items.append(item)
-    self.vault_manager.save_vault(self.items)
-    return self.items
-
-  def get_items(self):
-    return self.items
-
-  def delete_items(self, id: UUID):
-    self.items = [item for item in self.items if item.id != id]
-    self.vault_manager.save_vault(self.items)
-    return self.items
-
-  def update_items(self, update_item: VaultItem):
-    for ind, item in enumerate(self.items):
-      if item.id == update_item.id:
-        self.items[ind] = update_item
+    def add_item(self, item: VaultItem):
+        if self.state == State.LOCKED:
+            raise ValueError("Session is locked")
+        self.items.append(item)
         self.vault_manager.save_vault(self.items)
         return self.items
 
-    raise KeyError(f"Item with id={update_item.id} not found")
+    def get_items(self):
+        return self.items
+
+    def delete_items(self, id: UUID):
+        self.items = [item for item in self.items if item.id != id]
+        self.vault_manager.save_vault(self.items)
+        return self.items
+
+    def update_items(self, update_item: VaultItem):
+        for ind, item in enumerate(self.items):
+            if item.id == update_item.id:
+                self.items[ind] = update_item
+                self.vault_manager.save_vault(self.items)
+                return self.items
+
+        raise KeyError(f"Item with id={update_item.id} not found")
